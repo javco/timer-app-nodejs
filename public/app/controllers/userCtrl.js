0498fe2648cd87f7d0f87137acbf99cc271aed00
javi <jvrcastilla@gmail.com>
2017-08-28 22:09:39 +0200

    user create: add loading spinner, redirect to / and 2seg delay

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -1,7 +1,7 @@
 
 
 angular.module('userControllers', [])
-.controller('regCtrl', function($http){
+.controller('regCtrl', function($http, $location, $timeout){
     
     console.log('testing registration controller');
 
@@ -9,15 +9,23 @@ angular.module('userControllers', [])
 
     this.regUser = function(regData) {
         console.log('form submitted', this.regData);
+        
+        app.loading = true;
         app.errorMsg = false;
+
         $http.post('/api/users', this.regData).then(function(data){
             console.log(data);
             if(data.data.success){
+                app.loading = false;
                 //Create Success Message
                 app.successMsg = data.data.message;
                 //Redirect to Home page
+                $timeout(function(){
+                    $location.path('/');
+                }, 2000);
             }
             else{
+                app.loading = false;
                 //Create Error Message
                 app.errorMsg = data.data.message;
             }
